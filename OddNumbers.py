94f5918aaf41eafdf511645ca62a7bfdb343f916
Devashish5167 <156947721+Devashish5167@users.noreply.github.com>
2024-08-24 20:25:45 +0530

    Add files via upload

---
+++ OddNumbers.py
@@ -0,0 +1,8 @@
+# Odd numbers between two given numbers using while loop
+x = int(input("Enter min number: "))
+y = int(input("Enter max number: "))
+i=x
+if i%2 == 0: i=x+1
+while i<=y:
+    print(i)
+    i+=2
